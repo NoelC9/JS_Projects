a20c907ac4087bfb2a542cce46d7c23015dde139
NoelC9 <ncorrale@msudenver.edu>
2022-10-18 15:16:16 -0600

    trying to post request with fetch()

--- Node/index.js
+++ Node/index.js
@@ -1,4 +1,15 @@
+const { application } = require('express');
 const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'})); //allows server to parse incoming data as json.
+
+app.post('/api',(request, response) => {
+    console.log('I got a request');
+    console.log(request.body);
+    response.json({
+        status: 'success',
+        
+    })
+});
